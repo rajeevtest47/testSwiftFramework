994702abf4aa81ae0ee477a4d01fec8ec5da7264
Rajeev Rajeshuni <rajeev.rajeshuni@hansel.io>
2018-11-16 08:02:42 +0530

    Testing whether both .frameworks are created on bitrise

--- BuildScripts/postbuild.sh
+++ BuildScripts/postbuild.sh
@@ -5,15 +5,22 @@
 //  Created by Rajeev Rajeshuni on 15/11/18.
 //  Copyright © 2018 Rajeev Rajeshuni. All rights reserved.
 //
-
+iphoneos = "iphoneos"
 function copyFramework
 {
-    local finalFolder="archives"
+    local finalSimulatorFolder="archives/simulator/"
+    local finaliphoneOSFolder="archives/iphoneos/"
     local fileName="${BUILT_PRODUCTS_DIR}/${FULL_PRODUCT_NAME}"
     #local deviceBuildFile = "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PRODUCT_NAME}.framework"
     # local simulatorBuildFile =
-    rm -rf ${finalFolder}
-    mkdir -p ${finalFolder}
-    cp -a ${fileName} ${finalFolder}
+    if echo "${BUILT_PRODUCTS_DIR}" | grep -q "iphoneos"; then
+        rm -rf ${finaliphoneOSFolder};
+        mkdir -p ${finaliphoneOSFolder};
+        cp -a ${fileName} ${finaliphoneOSFolder};
+    else
+        rm -rf ${finalSimulatorFolder};
+        mkdir -p ${finalSimulatorFolder};
+        cp -a ${fileName} ${finalSimulatorFolder};
+    fi
 }
 copyFramework
